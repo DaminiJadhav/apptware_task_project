7112298e269d0c9f015ec5910bdc6f5a6d2ead35
DaminiJadhav <daminijadhav71@gmail.com>
2022-12-09 10:57:27 +0530

    add default files

---
+++ lib/DEFAULT/creditials.dart
@@ -0,0 +1,16 @@
+
+// https://app.pluralsight.com/
+// username: harish@apptware.com
+// password: iq3Xb4wrDw$t5TF
+
+// https://app.pluralsight.com/id
+// 5)ACr?rgS,3DS79
+
+
+//PF
+// 101881946385
+//251618624772
+
+//github token
+// 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
+// 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
---
+++ lib/DEFAULT/daily_task.dart
@@ -0,0 +1,117 @@
+//new laptop installation setup
+//solved dart version issue
+
+
+
+//28-10-2022
+// 1)Get image picker
+// 2)Store image in SQLite database
+// 3)Called with Rajas regarding RAAD healthcare project
+// 4)Checked RAAD Healthcare document
+
+
+//31-10-2022
+// 1)Solved emulator issue
+// 2)Solved image set issue
+// 3)Upload selected image from
+// 4)Handled add and edit images in add product screen
+// 5)Design changes(Set image instead of text and floation button position)
+
+//02/11/2022
+// Todays Task
+//1) Checked RAAG healthcare functinalities
+//2) Documents upload demo example
+
+//04/11/2022
+//- Whatup screen
+//    1) Designed list of chat user
+//    2) Designed Status screen
+//    3) Designed chat,call,status menu popup
+
+
+//07/11/2022
+// 1) Designed Whatsup chatting screen
+// 2) Solved chatting list ui issue
+
+//08/11/2022
+// 1) Worked on Story view functionality
+// 2) Working on upload story text screen
+
+//09/11/2022
+//- Whatsup Story text functionality
+// 1) Emoji functioanality(Done)
+// 2) Set random story background colors(Done)
+
+//10/11/2022
+// 1) Story text font change functionality
+// 2) Managed text Font,Emojis and change background color functinality
+// 2) Working on Camera ui
+
+//11/11/2022
+//1) Whatsup Camera
+//      - Designed main camera screen
+//     - Solved camera invalid range issue
+//     - Click photo using front back camera,camera flash on
+
+//14/11/2022
+// 1) Front,back video recording functinality
+// 2) Designed clicked photo,video main screen
+
+//15/11/2022
+//1) Unit testing(Inprogress)
+//2) Video playing demo app
+
+//16/11/2022
+//Video triming from gallary video and camera front,back recording
+
+//17/11/2022
+//1) Solved ffmpeg dependency issue
+//2) Laptop issue
+//3) Setup android,flutter
+
+//18/11/2022
+//Unit Testing demos
+
+//21/11/2022
+//Widget Testing demos
+
+//22/11/2022
+// 1)Integration Testing demos(Inprogress)
+// 2)Team meeting
+
+//23/11/2022
+// Integration testing (Inprogress)
+
+// 24/11/2022
+// Splash,Login screen intergration testing
+
+// 25/11/2022
+// 1) Testing Documentation
+// 2) Intergration Testing
+
+// 01/12/2022
+//Add to card animation
+
+// 02/12/2022
+//1) Toast animation
+//2) Validation unit test (Inprogress)
+
+//05/12/2022
+//1) Login validation using unit testing
+//2) Solved Validators class issue
+
+//06/12/2022
+//- Firebase test lab flutter demo(Inprogress)
+
+//07/12/2022
+//1) Firebase test lab flutter demo
+//2) R&D on Firebase crashlytic
+
+//08/12/2022
+// 1)R&D on Flutter web
+// 2)Flutter web application demo
+
+
+//1) Firebase crashlytic flutter demo
+//2) Debugginng using flutter tool
+//3) GOLDEN TEST
